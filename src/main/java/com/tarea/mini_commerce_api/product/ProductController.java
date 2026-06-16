ae2e10bbc3b6dbb2e7dd560936bd5413fcc12211
coco <meunse1999@gmail.com>
2026-06-16 15:40:35 +0900

    feat: 상품 단건, 목록 조회 기능 구현

--- src/main/java/com/tarea/mini_commerce_api/product/ProductController.java
+++ src/main/java/com/tarea/mini_commerce_api/product/ProductController.java
@@ -1,4 +1,34 @@
 package com.tarea.mini_commerce_api.product;
 
+import com.tarea.mini_commerce_api.product.dto.ProductRequestDto;
+import com.tarea.mini_commerce_api.product.dto.ProductResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponseDto register(@RequestBody @Valid ProductRequestDto dto) {
+        return productService.register(dto);
+    }
+
+    @GetMapping("/{productId}")
+    public ProductResponseDto getProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping
+    public List<ProductResponseDto> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
--- src/main/java/com/tarea/mini_commerce_api/product/ProductService.java
+++ src/main/java/com/tarea/mini_commerce_api/product/ProductService.java
@@ -2,11 +2,16 @@ package com.tarea.mini_commerce_api.product;
 
 import com.tarea.mini_commerce_api.product.dto.ProductRequestDto;
 import com.tarea.mini_commerce_api.product.dto.ProductResponseDto;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -16,7 +21,7 @@ public class ProductService {
 
     private final ProductRepository repository;
 
-    public ProductResponseDto register (Long productId, ProductRequestDto dto ) {
+    public ProductResponseDto register(ProductRequestDto dto) {
         log.info("상품 등록 시작 - productName ={}", dto.itemName());
         Product product = Product.builder()
                 .itemName(dto.itemName())
@@ -28,5 +33,22 @@ public class ProductService {
         Product saved = repository.save(product);
         log.info("상품 등록 완료 - productName ={}", dto.itemName());
         return ProductResponseDto.from(saved);
+    }
+
+    public ProductResponseDto getProduct(Long productId) {
+        log.info("상품 단건 조회 시작 - productId={}", productId);
+        Product product = repository.findById(productId)
+                .orElseThrow(()-> {
+                    log.warn("상품 단건 조회 실패 - productId={}", productId);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND,"상품을 찾을 수 없습니다. productId=" + productId);
+                        });
+        return ProductResponseDto.from(product);
+    }
+
+    public List<ProductResponseDto> getAllProducts() {
+        log.info("상품 목록 조회 시작");
+        return repository.findAll().stream()
+                .map(ProductResponseDto::from)
+                .toList();
     }
 }
